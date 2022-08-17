496e3fd4b1d7379209e2f39056f8a0518aae9ab6
djougue <anniechristelledjougue@gmail.com>
2022-08-17 14:55:37 +0100

    ajout de liste de tache

--- src/App.js
+++ src/App.js
@@ -1,8 +1,41 @@
+import {useState} from "react"
 
 function App() {
-  return (
-    <h1>DJOUGUE
-    </h1>
+    const[todos,settodos]=useState([
+      {tache:"aller au marche"},
+      {tache:"aller a l'ecole"},
+      {tache:"faire la lessive"},
+    ]);
+    const[value,setvalue]=useState("")
+
+    const traitementform=e =>{
+     e.preventDefault();
+
+     if(!value)return;
+      const newtodo=[...todos,{tache:value}]
+      settodos(newtodo);
+      setvalue('')
+      
+    };
+    
+    
+
+    return (
+      <div>
+         <form onSubmit={traitementform}>
+          <input type="text" value={value} onChange={e=>setvalue(e.target.value)} placeholder="entrez votre tache"
+          />
+         </form>
+          <hr/>
+          <h2> liste de tache ajoutee </h2>
+          {todos.map (todo=> (
+              <div> {todo.tache} </div>
+            ))
+          }
+       
+       
+
+      </div>
   );
 }
 
